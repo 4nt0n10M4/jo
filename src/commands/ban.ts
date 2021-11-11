0ec1cdbd62f95268e2c80cc1484f142cb182df3c
Vic <74506415+victorioxd@users.noreply.github.com>
2021-11-11 16:17:46 +0100

    🎨 Improve ban command code

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -17,21 +17,24 @@ class BanCommand extends Command {
         var reason = call.args.reason || "No reason provided."
         var sent: "✅"|"❎" = "❎"
         var appealbot: Boolean = false 
-        
+        var member: GuildMember | false 
+        var banMessage = `You have been banned from ${call.member.guild.name} \n Reason: "*${reason}*".`
+
         try {(await call.member.guild.members.fetch("868957041912320072")); appealbot = true } catch(e) { appealbot = false}
-        const member = await call.member.guild.members.fetch(call.args.target.id);
-        if (call.member.guild.ownerId == member.id ) return call.reply({content:"You can't ban the server owner."})
+        try {member = await call.member.guild.members.fetch(call.args.target.id);} catch (e) {member = false}
+
         if (!member) return call.reply({content:"I can't find the member."})
+        if (call.member.guild.ownerId == member.id ) return call.reply({content:"You can't ban the server owner."})
         if (!member.bannable) call.reply({content:"I can't ban this member."})
-        if (member.user.id == call.member.user.id) return call.reply({content:"I can't ban you xd."})
-        var banMessage = `You have been banned from ${call.member.guild.name} \n Reason: ${reason}`
+        if (member.user.id == call.member.user.id) return call.reply({content:"You can't ban yourself XD"})
         if (appealbot) banMessage += `\n Appeal here: https://appealbot.antonioma.com/g/${member.guild.id}/appeal `
-        if (member.roles.highest.position > call.member.roles.highest.position) return call.reply({content:"You need a highest position"})
+        if (member.roles.highest.position > call.member.roles.highest.position) return call.reply({content:"You need a highest position to ban this member."})
         await member.user.send(banMessage)
             .then(_=>sent = "✅")
             .catch(_=> sent = "❎")
         
         member.ban({reason})
+
         const embed = new MessageEmbed()
         embed.setTitle("Ban")
         embed.setAuthor(call.member.user.tag, call.member.displayAvatarURL())
