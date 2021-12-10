dd6f92306c87d39b378edef8b6b5dcfcc8e22248
Vic <74506415+victorioxd@users.noreply.github.com>
2021-12-10 18:18:07 +0100

    ✨ Add "cat" command (#13)
    * Add cat command
    
    * ✏️ Fix typo

---
+++ src/commands/cat.ts
@@ -0,0 +1,26 @@
+import axios from "axios"
+import { MessageEmbed } from "discord.js"
+import Command from "../types/Command"
+import CommandCall from "../types/CommandCall"
+
+class catCommand extends Command {
+    constructor() {
+        super({
+            description: "Return a random image of a cat.",
+            aliases: ["cat"],
+        })
+    }
+        async run (call: CommandCall) {
+            const img = await axios.get("https://thatcopy.pw/catapi/rest/")
+
+            const embed = new MessageEmbed()
+            .setTitle("Cat")
+            .setImage(img.data.url)
+            .setColor("#7289DA")
+            .setFooter("Powered by thatcopy.pw/catapi")
+
+            call.reply({embeds:[embed]})
+        }
+} 
+
+export default catCommand
