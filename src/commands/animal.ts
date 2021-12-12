a423d9e1da20e5bad7fda66bfb523d3f739bc69d
Antonio M A <antonioma@pm.me>
2021-12-12 20:20:53 +0100

    🎨 Fix intendentation

--- src/commands/animal.ts
+++ src/commands/animal.ts
@@ -9,46 +9,41 @@ class animalCommand extends Command {
         super({
             aliases: ["animal"],
             description: "Get an animal random image",
-            args: [
-            new CommandArg({
-            name: "name",
-            required: true,
-            type: "choice",
-            description:"The animal name",
-            choices: [["cat","Cat"], ["duck", "Duck"]]
-            })]
+            args: [new CommandArg({ name: "name", required: true, type: "choice", description:"The animal name", choices: [["cat","Cat"], ["duck", "Duck"]] })]
         })
-}
-
-async run(call: CommandCall) {
-    const { name } = call.args;
-    let img, source
-    switch (name) {
-        case "Cat":
-            img = (await (await axios.get("https://thatcopy.pw/catapi/rest/")).data.url)
-            source = "thatcopy.pw/catapi/"
-            
-            break;
-        case "Duck":
-            img = (await (await axios.get("https://random-d.uk/api/v2/random")).data.url)
-            source = "random-d.uk"
-
-            break;
-    
-        default:
-            img = "https://http.cat/404.jpg"
-            source = "https://http.cat/"
-            break;
     }
 
-    const embed = new MessageEmbed()
-    .setTitle(`${name}`)
-    .setImage(img)
-    .setFooter(`Powered by ${source}`)
-    .setColor("#0099ff")
+    async run(call: CommandCall) {
+        const { name } = call.args;
+        let img, source
 
-    call.reply({embeds:[embed]})
-}
+        switch (name) {
+            case "Cat":
+                img = (await (await axios.get("https://thatcopy.pw/catapi/rest/")).data.url)
+                source = "thatcopy.pw/catapi/"
+                
+                break;
+            case "Duck":
+                img = (await (await axios.get("https://random-d.uk/api/v2/random")).data.url)
+                source = "random-d.uk"
+
+                break;
+        
+            default:
+                img = "https://http.cat/404.jpg"
+                source = "https://http.cat/"
+                break;
+        }
+
+        const embed = new MessageEmbed()
+        .setTitle(`${name}`)
+        .setImage(img)
+        .setFooter(`Powered by ${source}`)
+        .setColor("#0099ff")
+
+        call.reply({embeds:[embed]})
+    }
+
 }
 
 export default animalCommand;
