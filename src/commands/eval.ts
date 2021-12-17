51cb97eb6fe20c52be62e3a8b1e248acf5bdeb83
Antonio M A <antonioma@pm.me>
2021-12-17 18:28:58 +0100

    🎨 Add support for codeblocks on eval and improve output by resolving promises

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,31 +1,49 @@
 import Command from '../types/Command';
 import CommandArg from '../types/CommandArg';
 import CommandCall from "../types/CommandCall";
+import isPromise from 'is-promise';
 
 class EvalCommand extends Command {
-    constructor(){
-        super({
-            aliases: ['eval'],
-            ownerOnly: true,
-            noPermsSilent: true,
-            hidden: true,
-            args: [new CommandArg({name: 'code', description: 'The code to eval', required: true, type: 'content'})]
-        });
-    }
+	constructor(){
+		super({
+			aliases: ['eval'],
+			ownerOnly: true,
+			noPermsSilent: true,
+			hidden: true,
+			args: [ new CommandArg({name: 'code', description: 'The code to eval', required: true, type: 'content'}) ]
+		});
+	}
 
-    run(call: CommandCall){
-        let response;
-        try{
-            response = eval(call.args.code);
-        } catch(e){
-            response = e;
-        }
-        if(typeof response !== 'string')response = JSON.stringify(response, null, 2);
+	async run(call: CommandCall){
+		let response;
+		let code = call.args.code;
 
-        response = response.replace(process.env.DISCORD_TOKEN+"", '');
+		if(code.startsWith('```') && code.endsWith('```')) {
+			code = code.trim().replace(/(^.*?\s)|(\n.*$)/g, '');
+		}
 
-        return call.reply({ content: `\`\`\`js\n${response.replace('\`', '\\\`')}\n\`\`\`` });
-    }
+		try {
+			response = eval(code);
+			if(Array.isArray(response)) { // Resolve any promises on arrays
+				let i = 0;
+				let responsearr = [];
+				for await (let r of response){
+					responsearr[i] = r; i++;
+				}
+				response = responsearr;
+			}
+
+			response = isPromise(response) ? await response : response;
+		} catch(err) {
+			return call.reply({ content: `Error while executing: \`${err}\`` });
+		}
+
+		if(typeof response !== 'string') response = JSON.stringify(response, null, 2);
+
+		response = response.replace(process.env.DISCORD_TOKEN+"", '');
+
+		return call.reply({ content: `\`\`\`js\n${response.replace('\`', '\\\`')}\n\`\`\`` });
+	}
 };
 
 export default EvalCommand;
