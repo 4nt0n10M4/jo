519ae7f1dd698d16b656574f50167290f3b67a2f
Antonio M A <antonioma@pm.me>
2021-12-18 21:22:10 +0100

    🎨 Improve format of `kick` and `ban` commands. (+ 🐛 bug fixes)
    * Force fetching of members on kick and ban commands to make sure permissions are updated.
    * Allow owner to kick and ban members even if his role is not high enough.

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -9,37 +9,38 @@ class KickCommand extends Command {
             aliases: ['kick'],
             description: 'Kick a user in your guild',
             permissions: ["KICK_MEMBERS"],
-            args: [new CommandArg({name: 'target', description: 'member to kick', required: true, type: "user"}), new CommandArg({name:"reason",description:"reason of the kick", required: false, type:"string"})]
+            args: [ new CommandArg({ name: 'target', description: 'Member to kick', required: true, type: "user" }), new CommandArg({ name:"reason", description: "Reason of the kick", required: false, type: "string" }) ]
         });
     }
 
     async run(call: CommandCall){
-        var reason = call.args.reason || "No reason provided."
-        var sent: "✅"|"❌" = "❌"
-        var member: GuildMember | false 
-        var kickMessage = `You have been kicked from ${call.member.guild.name} \n Reason: "*${reason}*".`
+        let reason = call.args.reason || "No reason provided.";
+        let sent = false;
+        let kickMessage = `You have been kicked from ${call.member.guild.name} \n Reason: "*${reason}*".`;
 
-        try {member = await call.member.guild.members.fetch(call.args.target.id);} catch (e) {member = false}
+        let targetMember: GuildMember | false = await call.member.guild.members.fetch({ user: call.args.target, force: true }).catch(_ => false); // force, because d.js permissions sometimes are not synced.
+        if (!targetMember) return call.reply({ content: "I can't find that member." });
 
-        if (!member) return call.reply({content:"I can't find that member."})
-        if (call.member.guild.ownerId == member.id ) return call.reply({content:"You can't kick the server owner."})
-        if (!member.kickable) return call.reply({content:"I can't kick that member."})
-        if (member.user.id == call.member.user.id) return call.reply({content:"You can't kick yourself XD"})
-        if (member.roles.highest.position > call.member.roles.highest.position) return call.reply({content:"You need a higher position to kick this member."})
-        await member.user.send(kickMessage)
-            .then(_=>sent = "✅")
-            .catch(_=> sent = "❌")
+        if (call.member.guild.ownerId == targetMember.id) return call.reply({ content: "You can't kick the server owner." });
+        if (!targetMember.kickable) return call.reply({ content: "I can't kick that member." });
+        if (targetMember.user.id == call.member.user.id) return call.reply({ content: "You can't kick yourself XD" });
+        if (targetMember.roles.highest.position > call.member.roles.highest.position && call.member.id !== call.member.guild.ownerId) return call.reply({ content: "You need a higher position to kick this member." });
+
+        await targetMember.user.send(kickMessage)
+            .then(_ => sent = true)
+            .catch(_ => sent = false);
         
-        member.kick(`${call.member.user.tag}: ${reason}`)
+        targetMember.kick(`${call.member.user.tag}: ${reason}`)
 
-        const embed = new MessageEmbed()
-        embed.setTitle("Kick")
-        embed.setAuthor(call.member.user.tag, call.member.displayAvatarURL())
-        embed.setTimestamp(new Date())
-        embed.setDescription(`${member.user.tag} has been kicked.`)
-        embed.addField("Reason", reason)
-            embed.addField("Kick message", sent)
-        return call.reply({embeds:[embed]})
+        let embed = new MessageEmbed()
+            .setTitle("Kick")
+            .setAuthor(call.member.user.tag, call.member.displayAvatarURL())
+            .setTimestamp(new Date())
+            .setDescription(`${targetMember.user.tag} has been kicked.`)
+            .addField("Reason", call.args.reason || "*No reason provided.*")
+            .addField("Kick message", sent ? "✅" : "❌");
+
+        return call.reply({ embeds: [embed] });
     }
 };
 export default KickCommand;
