5d07854367f633e11dc84509325a583902728b49
Vic <74506415+victorioxd@users.noreply.github.com>
2021-12-23 10:43:52 +0100

    ✨ Add date select

--- src/commands/apod.ts
+++ src/commands/apod.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import {  MessageEmbed } from "discord.js";
 import Command from "../types/Command";
+import CommandArg from "../types/CommandArg";
 import CommandCall from "../types/CommandCall";
 
 class apodCommmand extends Command {
@@ -8,14 +9,28 @@ class apodCommmand extends Command {
         super({
             aliases: ["apod"],
             description: "Get the Astronomy Picture of the Day",
-
+            args: [new CommandArg({name:"date", required: false, description: "Date of the picture", type: "string"})]
         })
     }
 
     async run(call: CommandCall) {
+        function formatDate(Originaldate: Date) {
+            var d = new Date(Originaldate),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+            if (month.length < 2) month = '0' + month;
+            if (day.length < 2) day = '0' + day;
+            return [year, month, day].join('-');
+        }
+        let dateString: string |undefined = undefined
+        if (call.args.date) {const date: Date | undefined = new Date(call.args.date)
+        if (date.toString() == "Invalid Date") return call.reply({content:"Invaild date."})
+        dateString = formatDate(date)
+        }
         const key = process.env.NASA_API_KEY ? process.env.NASA_API_KEY : "DEMO_KEY";
-        const {data} = await axios.get("https://api.nasa.gov/planetary/apod?api_key=" + key);
-
+        const {data} = dateString ? await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${dateString}`) : await axios.get("https://api.nasa.gov/planetary/apod?api_key=" + key);
+        
         const embed = new MessageEmbed()
         .setAuthor("Astronomy Picture of the Day")
         .setColor("#0099ff")
@@ -23,8 +38,7 @@ class apodCommmand extends Command {
         .setImage(data.url)
         .setFooter(data.date+" | "+data.copyright)
 
-        call.reply({embeds: [embed]});
-        
+        return call.reply({embeds: [embed]});
     }
 }
 
