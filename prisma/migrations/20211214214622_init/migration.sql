c55124ed083714efb94d3db65a9703b35a3be495
Restyled.io <commits@restyled.io>
2022-01-14 16:25:38 +0000

    Restyled by pg_format

--- prisma/migrations/20211214214622_init/migration.sql
+++ prisma/migrations/20211214214622_init/migration.sql
@@ -1,13 +1,15 @@
 -- CreateTable
 CREATE TABLE `Guild` (
     `id` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    PRIMARY KEY (`id`))
+DEFAULT CHARACTER
+SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+
 CREATE TABLE `User` (
     `id` VARCHAR(191) NOT NULL,
+    PRIMARY KEY (`id`))
+DEFAULT CHARACTER
+SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
